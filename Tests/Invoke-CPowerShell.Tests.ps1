4a29967006f5c67e9b552458b9669c35531fd179
Aaron Jensen <ajensen@webmd.net>
2023-05-17 11:46:41 -0700

    Migrating Invoke-CPowerShell tests to Pester 5.

--- Tests/Invoke-CPowerShell.Tests.ps1
+++ Tests/Invoke-CPowerShell.Tests.ps1
@@ -1,124 +1,109 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
-& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
+BeforeAll {
+    Set-StrictMode -Version 'Latest'
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
 
-$testCredential = Get-TestUser -Name 'CIPowerShell'
+    $testCredential = Get-TestUser -Name 'CIPowerShell'
 
-function Init
-{
-    $Global:Error.Clear()
-    $script:output = $null
-}
+    function ThenOutputIs
+    {
+        param(
+            [Object]$InputObject
+        )
 
-function ThenOutputIs
-{
-    param(
-        [Object]$InputObject
-    )
+        $output | Should -Be $InputObject
+    }
 
-    $output | Should -Be $InputObject
-}
+    function WhenRunningPowerShell
+    {
+        param(
+            [String[]]$WithArgs,
 
-function WhenRunningPowerShell
-{
-    param(
-        [String[]]$WithArgs,
+            [String]$Command,
 
-        [String]$Command,
+            [switch]$As32Bit,
 
-        [switch]$As32Bit,
+            [pscredential]$As
+        )
 
-        [pscredential]$As
-    )
-
-    # Make tests a little faster and reduce test code clutter.
-    $WithArgs = & {
-        '-NoProfile'
-        '-NonInteractive'
-        if( $WithArgs )
-        {
-            $WithArgs
+        # Make tests a little faster and reduce test code clutter.
+        $WithArgs = & {
+            '-NoProfile'
+            '-NonInteractive'
+            if( $WithArgs )
+            {
+                $WithArgs
+            }
         }
+
+        $conditionalParams = @{}
+        if( $Command )
+        {
+            $conditionalParams['Command'] = $Command
+        }
+
+        if( $As32Bit )
+        {
+            $conditionalParams['x86'] = $true
+        }
+
+        if( $As )
+        {
+            $conditionalParams['Credential'] = $As
+        }
+
+        $script:output = Invoke-CPowerShell -ArgumentList $WithArgs @conditionalParams
     }
 
-    $conditionalParams = @{}
-    if( $Command )
+    function Assert-EnvVarCleanedUp
     {
-        $conditionalParams['Command'] = $Command
-    }
-
-    if( $As32Bit )
-    {
-        $conditionalParams['x86'] = $true
-    }
-
-    if( $As )
-    {
-        $conditionalParams['Credential'] = $As
-    }
-
-    $script:output = Invoke-CPowerShell -ArgumentList $WithArgs @conditionalParams
-}
-
-function Assert-EnvVarCleanedUp
-{
-    It 'should clean up environment' {
-        ([Environment]::GetEnvironmentVariable('COMPLUS_ApplicationMigrationRuntimeActivationConfigPath')) | Should -BeNullOrEmpty
+        It 'should clean up environment' {
+            ([Environment]::GetEnvironmentVariable('COMPLUS_ApplicationMigrationRuntimeActivationConfigPath')) | Should -BeNullOrEmpty
+        }
     }
 }
 
 Describe 'Invoke-CPowerShell.when requesting a 32-bit PowerShell' {
-    if( (Test-COperatingSystem -IsWindows) -and (Test-CPowerShell -IsDesktop) )
-    {
-        Context 'On Windows' {
-            It 'should run under x86' {
-                Init
-                WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::Is64BitProcess"' -As32Bit
-                ThenOutputIs $false.ToString()
-            }
-        }
+    BeforeEach {
+        $Global:Error.Clear()
+        $script:output = $null
     }
-    else
-    {
-        Context 'On PowerShell Core' {
-            It 'should run under same architecture as this test' {
-                Init
-                WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::Is64BitProcess"' -As32Bit
-                ThenOutputIs ([Environment]::Is64BitProcess)
-            }
-        }
-    }
-}
 
-if( (Test-COperatingSystem -IsWindows) -and (Test-CPowerShell -IsDesktop) )
-{
-    Describe 'Invoke-CPowerShell.when running on Windows' {
-        if( (Test-COperatingSystem -Is64Bit) ) 
-        {
-            Context 'x64' {
-                Context 'from x86 PowerShell' {
-                    It 'should run x64 PowerShell by default' {
-                        Init
-                        if( (Test-CPowerShell -Is32Bit) )
-                        {
-                            WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::Is64BitProcess"'
-                        }
-                        else
-                        {
-                            $command = @"
+    if (-not (Test-Path -Path 'variable:IsWindows'))
+    {
+        Set-Variable -Name 'IsWindows' -Value $true -Scope Local
+        Set-Variable -Name 'IsLinux' -Value $false -Scope Local
+        Set-Variable -Name 'IsMacOS' -Value $false -Scope Local
+    }
+
+    $IsDesktop = $PSVersionTable['PSEdition'] -eq 'Desktop'
+    $IsCore = -not $IsDesktop
+
+    Context 'On PowerShell Core' -Skip:(-not $IsCore) {
+        It 'should run under same architecture as this test' {
+            WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::Is64BitProcess"' -As32Bit
+            ThenOutputIs ([Environment]::Is64BitProcess)
+        }
+    }
+
+    Context 'On Windows' -Skip:(-not $IsWindows) {
+        It 'runs under x86' {
+            WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::Is64BitProcess"' -As32Bit
+            ThenOutputIs $false.ToString()
+        }
+        Context 'x64' -Skip:(-not [Environment]::Is64BitOperatingSystem) {
+            Context 'from x86 PowerShell' {
+                It 'runs x64 PowerShell by default' {
+                    if( (Test-CPowerShell -Is32Bit) )
+                    {
+                        WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::Is64BitProcess"'
+                    }
+                    else
+                    {
+                        $command = @"
 Import-Module "$(Join-Path -Path $PSScriptRoot -ChildPath '..\Carbon.Core' -Resolve)"
 if( -not (Test-CPowerShell -Is32Bit) )
 {
@@ -126,21 +111,20 @@ if( -not (Test-CPowerShell -Is32Bit) )
 }
 Invoke-CPowerShell -ArgumentList '-NoProfile', '-NonInteractive', '[Environment]::Is64BitProcess'
 "@
-                            WhenRunningPowerShell -Command $command -As32Bit
-                        }
-                        ThenOutputIs $true
+                        WhenRunningPowerShell -Command $command -As32Bit
                     }
+                    ThenOutputIs $true
                 }
-                Context 'from x64 PowerShell' {
-                    It 'should run x64 PowerShell' {
-                        Init
-                        if( (Test-CPowerShell -Is64Bit) )
-                        {
-                            WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::Is64BitProcess"'
-                        }
-                        else
-                        {
-                            $command = @"
+            }
+            Context 'from x64 PowerShell' {
+                It 'should run x64 PowerShell' {
+                    if( (Test-CPowerShell -Is64Bit) )
+                    {
+                        WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::Is64BitProcess"'
+                    }
+                    else
+                    {
+                        $command = @"
 Import-Module "$(Join-Path -Path $PSScriptRoot -ChildPath '..\Carbon.Core' -Resolve)"
 if( -not (Test-CPowerShell -Is64Bit) )
 {
@@ -148,27 +132,17 @@ if( -not (Test-CPowerShell -Is64Bit) )
 }
 return [Environment]::Is64BitPowerShell
 "@
-                            WhenRunningPowerShell -Command $command
-                        }
-                        ThenOutputIs $true
+                        WhenRunningPowerShell -Command $command
                     }
+                    ThenOutputIs $true
                 }
             }
         }
     }
-}
 
-# On macOS/Linux, there's a bug in Start-Job that prevents it from launching as another user.
-# https://github.com/PowerShell/PowerShell/issues/7172
-$skip = @{}
-if( -not (Test-COperatingSystem -IsWindows) )
-{
-    $skip['Skip'] = $true
-}
-
-Describe 'Invoke-CPowerShell.when running a script as another user' {
-    It 'should run PowerShell as that user' @skip {
-        Init
+    # On macOS/Linux, there's a bug in Start-Job that prevents it from launching as another user.
+    # https://github.com/PowerShell/PowerShell/issues/7172
+    It 'run PowerShell as another user' -Skip:(-not $IsWindows) {
         WhenRunningPowerShell -WithArgs '-Command', '"[Environment]::UserName"' -As $testCredential
         ThenOutputIs $testCredential.UserName
     }

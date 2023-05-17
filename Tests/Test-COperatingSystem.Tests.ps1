2d2c7c6dfe1c8c2e6121e3f6854307ce4054882e
Aaron Jensen <ajensen@webmd.net>
2023-05-17 15:51:59 -0700

    Migrating Test-COperatingSystem tests to Pester 5.

--- Tests/Test-COperatingSystem.Tests.ps1
+++ Tests/Test-COperatingSystem.Tests.ps1
@@ -2,108 +2,99 @@
 #Requires -Version 5.1
 Set-StrictMode -Version 'Latest'
 
-& (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
+BeforeAll {
+    Set-StrictMode -Version 'Latest'
 
-$onWindows = $true
-$onLinux = $false
-$onMac = $false
-
-if( (Test-Path -Path 'variable:IsWindows') )
-{
-    $onWindows = $IsWindows
-    $onLinux = $IsLinux
-    $onMac = $IsMacOS
+    & (Join-Path -Path $PSScriptRoot -ChildPath 'Initialize-Test.ps1' -Resolve)
 }
 
 Describe 'Test-COperatingSystem' {
-    if( [Environment]::Is64BitOperatingSystem )
+    $onWindows = $true
+    $onLinux = $false
+    $onMac = $false
+
+    if( (Test-Path -Path 'variable:IsWindows') )
     {
-        Context 'OS x64' {
-            Context 'when testing if OS is x64' {
-                It 'should return $true' {
-                    Test-COperatingSystem -Is64Bit | Should -BeTrue
-                }
-            }
-            Context 'when testing if OS is x86' {
-                It 'should return $false' {
-                    Test-COperatingSystem -Is32Bit | Should -BeFalse
-                }
+        $onWindows = $IsWindows
+        $onLinux = $IsLinux
+        $onMac = $IsMacOS
+    }
+
+    Context 'OS x64' -Skip:(-not [Environment]::Is64BitOperatingSystem) {
+        Context 'when testing if OS is x64' {
+            It 'should return $true' {
+                Test-COperatingSystem -Is64Bit | Should -BeTrue
             }
         }
-    }
-    else
-    {
-        Context 'OS x86' {
-            Context 'when testing if OS is x64' {
-                It 'should return $false' {
-                    Test-COperatingSystem -Is64Bit | Should -BeFalse
-                }
-            }
-            Context 'when testing if OS is x86' {
-                It 'should return $true' {
-                    Test-COperatingSystem -Is32Bit | Should -BeTrue
-                }
+        Context 'when testing if OS is x86' {
+            It 'should return $false' {
+                Test-COperatingSystem -Is32Bit | Should -BeFalse
             }
         }
     }
 
-    if( $onWindows )
-    {
-        Context 'Windows' {
-            Context 'when testing if OS is Linux' {
-                It 'should return $false' {
-                    Test-COperatingSystem -IsLinux | Should -BeFalse
-                }
+    Context 'OS x86' -Skip:([Environment]::Is64BitOperatingSystem) {
+        Context 'when testing if OS is x64' {
+            It 'should return $false' {
+                Test-COperatingSystem -Is64Bit | Should -BeFalse
             }
-            Context 'when testing if OS is macOS' {
-                It 'should return $false' {
-                    Test-COperatingSystem -IsMacOS | Should -BeFalse
-                }
-            }
-            Context 'when testing if OS is Windows' {
-                It 'should return $true' {
-                    Test-COperatingSystem -IsWindows | Should -BeTrue
-                }
+        }
+        Context 'when testing if OS is x86' {
+            It 'should return $true' {
+                Test-COperatingSystem -Is32Bit | Should -BeTrue
             }
         }
     }
-    elseif( $onLinux )
-    {
-        Context 'Linux' {
-            Context 'when testing if OS is Linux' {
-                It 'should return $true' {
-                    Test-COperatingSystem -IsLinux | Should -BeTrue
-                }
+
+    Context 'Windows' -Skip:(-not $onWindows) {
+        Context 'when testing if OS is Linux' {
+            It 'should return $false' {
+                Test-COperatingSystem -IsLinux | Should -BeFalse
             }
-            Context 'when testing if OS is macOS' {
-                It 'should return $false' {
-                    Test-COperatingSystem -IsMacOS | Should -BeFalse
-                }
+        }
+        Context 'when testing if OS is macOS' {
+            It 'should return $false' {
+                Test-COperatingSystem -IsMacOS | Should -BeFalse
             }
-            Context 'when testing if OS is Windows' {
-                It 'should return $False' {
-                    Test-COperatingSystem -IsWindows | Should -BeFalse
-                }
+        }
+        Context 'when testing if OS is Windows' {
+            It 'should return $true' {
+                Test-COperatingSystem -IsWindows | Should -BeTrue
             }
         }
     }
-    elseif( $onMac )
-    {
-        Context 'macOS' {
-            Context 'when testing if OS is Linux' {
-                It 'should return $false' {
-                    Test-COperatingSystem -IsLinux | Should -BeFalse
-                }
+
+    Context 'Linux' -Skip:(-not $onLinux) {
+        Context 'when testing if OS is Linux' {
+            It 'should return $true' {
+                Test-COperatingSystem -IsLinux | Should -BeTrue
             }
-            Context 'when testing if OS is macOS' {
-                It 'should return $true' {
-                    Test-COperatingSystem -IsMacOS | Should -BeTrue
-                }
+        }
+        Context 'when testing if OS is macOS' {
+            It 'should return $false' {
+                Test-COperatingSystem -IsMacOS | Should -BeFalse
             }
-            Context 'when testing if OS is Windows' {
-                It 'should return $false' {
-                    Test-COperatingSystem -IsWindows | Should -BeFalse
-                }
+        }
+        Context 'when testing if OS is Windows' {
+            It 'should return $False' {
+                Test-COperatingSystem -IsWindows | Should -BeFalse
+            }
+        }
+    }
+    Context 'macOS' -Skip:(-not $onMac) {
+        Context 'when testing if OS is Linux' {
+            It 'should return $false' {
+                Test-COperatingSystem -IsLinux | Should -BeFalse
+            }
+        }
+        Context 'when testing if OS is macOS' {
+            It 'should return $true' {
+                Test-COperatingSystem -IsMacOS | Should -BeTrue
+            }
+        }
+        Context 'when testing if OS is Windows' {
+            It 'should return $false' {
+                Test-COperatingSystem -IsWindows | Should -BeFalse
             }
         }
     }
